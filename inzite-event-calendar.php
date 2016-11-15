38d90b1319c8782a07fb831c6223e0fcc438077d
Rasmus Jürs <rasmus@jurs.me>
2016-11-15 09:39:01 +0100

    Fix typo

--- inzite-event-calendar.php
+++ inzite-event-calendar.php
@@ -3,7 +3,7 @@
 Plugin Name: Inzite Event Calendar
 Description: Show events in a calendar.
 Author: Johnnie Berthelsen & Rasmus Jürs
-Verson: 1.0.1
+Version: 1.0.1
 Author URI: http://inzite.dk
 */
 
